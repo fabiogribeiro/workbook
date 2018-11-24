37d6dac14243b542d7746c460f0c81adf64a4c52
Fábio Ribeiro <louiskkf@gmail.com>
2018-11-24 12:29:19 +0000

    Create challenges migration

---
+++ database/migrations/2018_11_24_121012_create_challenges_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateChallengesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('challenges', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('title');
+            $table->text('body');
+            $table->string('answer');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('challenges');
+    }
+}
