10e91b652c70d14a0e83996d6ec67befab5bcfb1
Fábio Ribeiro <louiskkf@gmail.com>
2018-12-14 19:57:46 +0000

    Add a few translations

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -13,6 +13,8 @@ return [
     |
     */
 
+    'register' => 'Register',
+    'name' => 'Name',
     'failed' => 'These credentials do not match our records.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
 
---
+++ resources/lang/pt/auth.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+  'register' => 'Registar',
+  'name' => 'Nome',
+];
