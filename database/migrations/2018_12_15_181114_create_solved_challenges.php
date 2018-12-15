7a410f6368317bea55c6fff00dacb32c52a680ed
Fábio Ribeiro <louiskkf@gmail.com>
2018-12-15 18:24:07 +0000

    Add a solved challenges column to user table

---
+++ database/migrations/2018_12_15_181114_create_solved_challenges.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSolvedChallenges extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->json('solved_challenges')->default('[]');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('solved_challenges');
+        });
+    }
+}
