3cbb41ced56111f77ec29bddc50a16c7458b20fe
Fábio Ribeiro <louiskkf@gmail.com>
2019-01-04 09:04:41 +0000

    Add asset versioning on production

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,20 @@ const mix = require('laravel-mix');
  |
  */
 
+/**
+ * Compile js/css assets to public
+ */
 mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
+    .sass('resources/sass/app.scss', 'public/css');
 
+/**
+ * Bust cache when changes happen in production
+ */
+if (mix.inProduction()) {
+   mix.version();
+}
+
+/**
+ * Use browserSync on development server
+ */
 mix.browserSync('localhost:8000');
