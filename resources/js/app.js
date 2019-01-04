74a0a30f678cebb9a2f62319614007917bb3ef65
Fábio Ribeiro <louiskkf@gmail.com>
2019-01-04 15:22:00 +0000

    Bump laravel-mix version

--- resources/js/app.js
+++ resources/js/app.js
@@ -19,11 +19,10 @@ window.Vue = require('vue');
 
 //Vue.component('example-component', require('./components/ExampleComponent.vue'));
 
-const files = require.context('./', true, /\.vue$/i)
 
-files.keys().map(key => {
-    return Vue.component(_.last(key.split('/')).split('.')[0], files(key))
-})
+const files = require.context('./', true, /\.vue$/i);
+
+files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
