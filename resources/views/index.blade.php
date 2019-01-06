e496776c0ffb371bd5033e03ade32e3bfb94a432
Fábio Ribeiro <louiskkf@gmail.com>
2019-01-06 23:57:04 +0000

    Fix whitespace in front page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="col-12 row">
+<div class="col-12">
   <div class="mx-xs-auto text-center text-md-left col-md-4 offset-md-1 home">
     <h1>Learn science</h1>
     <p>Challenges in math, physics and chemistry to learn the theory behind the experiments.</p>
@@ -11,10 +11,6 @@
       </button>
     </a>
   </div>
-</div>
-
-<div class="index-subjects-container">
-  @include('partials.subjects')
 </div>
 
 @endsection
