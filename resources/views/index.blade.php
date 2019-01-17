a4e8575965f212960be04cb06d6c290f895885ae
Fábio Ribeiro <louiskkf@gmail.com>
2019-01-17 20:59:57 +0000

    Add a register panel in the home page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,15 +2,28 @@
 
 @section('content')
 <div class="col-12">
-  <div class="mx-xs-auto text-center text-md-left col-md-4 offset-md-1 home">
+  <div class="d-inline-block mx-xs-auto text-center text-md-left col-md-4 offset-md-1 home-panel">
     <h1>Learn science</h1>
     <p>Challenges in math, physics and chemistry to learn the theory behind the experiments.</p>
       <a href="{{ route('dashboard') }}">
-      <button class="btn btn-lg btn-success">
-        Get started
-      </button>
-    </a>
+        <button class="btn btn-lg btn-success">
+          Get started
+        </button>
+      </a>
+  </div>
+
+  <div class="d-inline-block mx-xs-auto text-center text-md-left col-md-4 offset-md-1 home-panel">
+    <div class="home-register">
+      <h2>First time visiting?</h2>
+      <h4>Create an account to save your progress</h4>
+      <input class="w-75" placeholder="Email" type="email" />
+      <input class="w-75" placeholder="Password" type="password" />
+      <a href="{{ route('dashboard') }}">
+        <button class="btn btn-primary">
+          Register
+        </button>
+      </a>
+    </div>
   </div>
 </div>
-
 @endsection
