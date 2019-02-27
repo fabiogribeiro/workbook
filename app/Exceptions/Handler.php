a5558df6bf3b2cc9bcf45f5f7f91ccca62a8273a
Fábio Ribeiro <louiskkf@gmail.com>
2019-02-27 20:09:05 +0000

    Always redirect to /register when register data is submitted

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -46,6 +46,12 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        if ($exception instanceof \Illuminate\Validation\ValidationException &&
+            $request->path() == 'register') {
+
+                $exception->redirectTo = 'register';
+        }
+
         return parent::render($request, $exception);
     }
 }
