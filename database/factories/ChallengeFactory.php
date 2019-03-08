14e0b82c95f61c44bf06e89f611e31d47ec95de1
Fábio Ribeiro <louiskkf@gmail.com>
2019-03-08 20:00:58 +0000

    Seed challenge body with variable number of paragraphs

--- database/factories/ChallengeFactory.php
+++ database/factories/ChallengeFactory.php
@@ -5,7 +5,7 @@ use Faker\Generator as Faker;
 $factory->define(App\Challenge::class, function (Faker $faker) {
     return [
         'title' => $faker->sentence(2, true),
-        'body' => $faker->paragraph(4, true),
+        'body' => $faker->paragraphs($faker->randomNumber(1), true),
         'answer' => $faker->randomElement(array('A', 'B', 'C', 'D')),
         'skill' => $faker->randomElement(array('Skill 1', 'Skill 2', 'Skill 3', 'Skill 4')),
         'slug' => function(array $challenge) {
