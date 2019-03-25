f5219420832715f77f0a57ed4d69a18335680cf4
Fábio Ribeiro <louiskkf@gmail.com>
2019-03-25 18:03:13 +0000

    Use flex layout for challenge list

--- resources/js/components/ChallengesList.vue
+++ resources/js/components/ChallengesList.vue
@@ -2,14 +2,18 @@
 <div class="col-md-8 col-lg-9">
   <div class="default-card" v-for="(challenges, skill) in apiData" :key="skill">
     <h3>{{ skill }} </h3>
-    <ul class="list-group list-group-flush">
-      <li v-for="challenge in challenges" :key="challenge.id" class="list-group-item">
-        <a v-bind:href="baseUrl + challenge.slug">
-          <p class="overflow-ellipsis">{{ challenge.title }}</p>
-          <button class="btn btn-outline-success float-right"> Solve </button>
-        </a>
-      </li>
-    </ul>
+    <div class="list-group list-group-flush">
+      <a
+        class="list-group-item"
+        v-for="challenge in challenges" :key="challenge.id"
+        v-bind:href="baseUrl + challenge.slug"
+      >
+        <div class="d-flex justify-content-between align-items-end w-100">
+          <h5>{{ challenge.title }} </h5>
+          <button class="btn btn-outline-success float-right">Solve</button>
+        </div>
+      </a>
+    </div>
   </div>
 </div>
 </template>
@@ -26,23 +30,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-li {
-  font-size: 1.5rem;
-
-  p {
-    display: inline-block;
-    margin-bottom: 0;
-    max-width: 70%;
-    vertical-align: top;
-  }
-
-  a {
-    display: block;
-  }
-}
-
 button {
-  display: inline-block;
   width: 25%;
 }
 
@@ -56,7 +44,5 @@ h3 {
   margin-top: 0.7rem;
   margin-left: 1.25rem;
   margin-bottom: 1.4rem;
-  border-bottom: 2px solid #38c172;
-  display: inline-block;
 }
 </style>
