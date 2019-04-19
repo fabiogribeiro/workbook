3ca3c74a9a163e22d1afeb81183ac290394f0b2d
Fábio Ribeiro <louiskkf@gmail.com>
2019-04-19 09:57:24 +0100

    Add input picker item

--- resources/js/components/SolveChallengeForm.vue
+++ resources/js/components/SolveChallengeForm.vue
@@ -1,5 +1,15 @@
 <template>
-  <div class="pt-3 col-md-6 float-right">
+  <div class="pt-3 row">
+    <div class="col-md-6 my-auto">
+      <ul class="list-group list-group-horizontal justify-content-center">
+        <li v-for="i in numberInputs"
+            v-bind:key="i"
+            class="list-group-item">
+          <a v-bind:class="{active: i === activeInput}" href="#">{{ i }}</a>
+        </li>
+      </ul>
+    </div>
+  <div class="col-md-6">
     <form v-on:submit.prevent="checkAnswer">
       <div class="form-group row">
         <label for="answer" class="col-sm-3 col-form-label">
@@ -14,6 +24,7 @@
       </button>
     </form>
   </div>
+  </div>
 </template>
 
 <script>
@@ -21,7 +32,9 @@ export default {
   props: ['challengeId', 'formLabel', 'submitLabel'],
   data: function() {
     return {
-      userInput: ''
+      userInput: '',
+      numberInputs: 5,
+      activeInput: 2,
     }
   },
   methods: {
@@ -41,3 +54,27 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+li {
+  margin-left: 8px;
+  margin-right: 8px;
+  padding: 0;
+  border: 0;
+
+  a {
+    display: flex;
+    padding: 0.4rem 0.9rem;
+    border-radius: 50% !important;
+
+    &:hover {
+      background-color: #eaecef;
+    }
+
+    &.active {
+      background-color: #3490dc;
+      color: #fff;
+    }
+  }
+}
+</style>
