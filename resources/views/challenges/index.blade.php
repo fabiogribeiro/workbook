eb509e9cbeb430513b4187b1d9e3af30f2680146
Fábio Ribeiro <louiskkf@gmail.com>
2019-05-02 13:00:41 +0100

    Remove unused view

--- resources/views/challenges/index.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-    <challenges-list
-        v-bind:api-data="{{ json_encode($challengesBySkill) }}"
-        v-bind:active-item="{{ json_encode($activeSubject) }}"
-    ></challenges-list>
-@endsection
