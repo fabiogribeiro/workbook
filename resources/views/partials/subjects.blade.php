2bb7cba9f98e8423dd1b35c60098b4119671a023
Fábio Ribeiro <louiskkf@gmail.com>
2019-05-02 12:30:36 +0100

    Simplify subjects list

--- resources/views/partials/subjects.blade.php
+++ resources/views/partials/subjects.blade.php
@@ -3,22 +3,18 @@
     <h1>Learn, practice or solve challenges</h1>
   </div>
 
-  @component('components.wideheader')
-    ALL DOMAINS
-  @endcomponent
+  @foreach ($subjects as $domain => $subjectList)
+    @component('components.wideheader')
+      {{ strtoupper($domain) }}
+    @endcomponent
 
-  <div class="subjects-panel">
-    @foreach ($subjects as $domain => $subjectList)
-    <div class="subjects-item">
-      <h1> {{ title_case(__('domains.' . $domain)) }} </h1>
-      <ul class="list-inline">
-        @foreach ($subjectList as $subject)
-        <li class="list-inline-item">
-          <a href="{{ route('challenges.index', ['domain' => $domain, 'subject' => $subject->slug]) }}">{{ $subject->title }}</a>
-        </li>
-        @endforeach
-      </ul>
-    </div>
+    @foreach ($subjectList as $subject)
+      <a
+        href="{{ route('challenges.index', ['domain' => $domain,
+                                            'subject' => $subject->slug]) }}"
+      >
+        {{ $subject->title }}
+      </a>
     @endforeach
-  </div>
+  @endforeach
 </div>
