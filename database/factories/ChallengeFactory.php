de4615fd7fbe63b497304044b30191b8934a9c21
Fábio Ribeiro <louiskkf@gmail.com>
2019-05-10 21:02:18 +0100

    Create Faker question provider

--- database/factories/ChallengeFactory.php
+++ database/factories/ChallengeFactory.php
@@ -1,15 +1,18 @@
 <?php
 
 use Faker\Generator as Faker;
+use App\Faker\Question;
 
 $factory->define(App\Challenge::class, function (Faker $faker) {
+    $faker->addProvider(new Question($faker));
+
     return [
         'title' => $faker->sentence(2, true),
         'body' => $faker->paragraphs($faker->randomNumber(1), true),
         'body_html' => function(array $challenge) {
             return (new Parsedown())->text($challenge['body']);
         },
-        'answer' => $faker->randomElement(array('A', 'B', 'C', 'D')),
+        'questions' => json_encode($faker->questions(3)),
         'skill' => $faker->randomElement(array('Skill 1', 'Skill 2', 'Skill 3', 'Skill 4')),
         'slug' => function(array $challenge) {
             return str_slug($challenge['title']);
