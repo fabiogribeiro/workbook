e8bafca8f8bf7746b45811851ee6fabdb70b8f13
Fábio Ribeiro <louiskkf@gmail.com>
2019-05-25 04:26:21 +0100

    Put questions on their own table

---
+++ database/migrations/2019_05_25_031524_move_questions_to_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class MoveQuestionsToTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('challenges', function (Blueprint $table) {
+            $table->dropColumn('questions');
+        });
+
+        Schema::create('questions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->string('title');
+            $table->jsonb('question_data')->default('[]');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('questions');
+
+        Schema::table('challenges', function (Blueprint $table) {
+            $table->jsonb('questions')->default('[]');
+        });
+    }
+}
