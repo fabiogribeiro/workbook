bafa19f0e443eb1d303bb6269b874b011e6b5ac2
Fábio Ribeiro <louiskkf@gmail.com>
2019-05-26 01:36:27 +0100

    Use uikit for navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,90 +30,65 @@
 </head>
 <body>
   <div id="app">
-    <nav class="py-2 navbar navbar-expand-md
-      {{ Request::is('/') ? 'navbar-light' : 'navbar-dark dark-bg' }}">
-      <div class="container">
-        <a class="navbar-brand" href="{{ url('/') }}">
-          {{ config('app.name', 'Laravel') }}
-        </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-          <span class="navbar-toggler-icon"></span>
-        </button>
+    <div class="uk-container">
+      <nav class="uk-navbar" uk-navbar="delay-hide: 200">
+        <div class="uk-navbar-left">
+          <a class="uk-navbar-item uk-logo" href="{{ url('/') }}">
+            {{ config('app.name', 'Laravel') }}
+          </a>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <!-- Left Side Of Navbar -->
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item dropdown">
-              <a id="changeLangDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                {{ strtoupper(app()->getLocale()) }} <span class="caret"></span>
+          <ul class="uk-navbar-nav">
+            <li>
+              <a href="#">
+                {{ app()->getLocale() }}
               </a>
-
-              <div class="dropdown-menu dropdown-menu-left" aria-labelledby="changeLangDropdown">
-                <a class="dropdown-item" href="{{ route('lang') }}"
-                  onclick="event.preventDefault();
-                  document.getElementById('lang-input').value = 'en';
-                  document.getElementById('lang-form').submit();">
-                  English
-                </a>
-
-                <a class="dropdown-item" href="{{ route('lang') }}"
-                  onclick="event.preventDefault();
-                  document.getElementById('lang-input').value = 'pt';
-                  document.getElementById('lang-form').submit();">
-                  Português
-                </a>
-
-                <form id="lang-form" action="{{ route('lang') }}" method="POST" style="display: none;">
-                  @method('PUT')
-                  @csrf
-
-                  <input id="lang-input" type="text" name="lang">
-                </form>
+              <div class="uk-navbar-dropdown">
+                <ul class="uk-nav uk-navbar-dropdown-nav">
+                  <li>
+                    <a href="#"
+                      onclick="event.preventDefault();
+                      document.getElementById('lang-input').value = 'en';
+                      document.getElementById('lang-form').submit();">
+                      English
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#"
+                      onclick="event.preventDefault();
+                      document.getElementById('lang-input').value = 'pt';
+                      document.getElementById('lang-form').submit();">
+                      Português
+                    </a>
+                  </li>
+                </ul>
               </div>
-            </li>
-          </ul>
+              <form id="lang-form" action="{{ route('lang') }}" method="POST" style="display: none;">
+                @method('PUT')
+                @csrf
 
-          <!-- Right Side Of Navbar -->
-          <ul class="navbar-nav ml-auto">
-            <!-- Authentication Links -->
-            @guest
-            <li class="nav-item">
-              <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                <input id="lang-input" type="text" name="lang">
+              </form>
             </li>
-            <li class="nav-item">
-              @if (Route::has('register'))
-              <a class="nav-link" href="{{ route('register') }}">{{ __('auth.register') }}</a>
-              @endif
-            </li>
-            @else
-            <li class="nav-item dropdown">
-              <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                {{ Auth::user()->name }} <span class="caret"></span>
-              </a>
-
-              <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                <a class="dropdown-item" href="{{ route('profile') }}">
-                  {{ __('Profile') }}
-                </a>
-
-                <a class="dropdown-item" href="{{ route('logout') }}"
-                  onclick="event.preventDefault();
-                  document.getElementById('logout-form').submit();">
-                  {{ __('Logout') }}
-                </a>
-
-                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                  @csrf
-                </form>
-              </div>
-            </li>
-            @endguest
           </ul>
         </div>
-      </div>
-    </nav>
+        <div class="uk-navbar-right">
+          <ul class="uk-navbar-nav">
+            <li>
+              <a href="{{ route('login') }}">
+                {{ __('Login') }}
+              </a>
+            </li>
+            <li>
+              <a href="{{ route('register') }}">
+                {{ __('auth.register') }}
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
 
-    <main class="py-4 {{ Request::is('/') ? '' : 'gray-bg' }}">
+    <main class="{{ Request::is('/') ? '' : 'gray-bg' }}">
       @yield('content')
     </main>
   </div>
