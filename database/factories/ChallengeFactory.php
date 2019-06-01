ca465238c9adb54def51790600cf5355746ffcae
Fábio Ribeiro <louiskkf@gmail.com>
2019-06-01 03:32:46 +0100

    Remove unnecessary faker question provider

--- database/factories/ChallengeFactory.php
+++ database/factories/ChallengeFactory.php
@@ -1,18 +1,14 @@
 <?php
 
 use Faker\Generator as Faker;
-use App\Faker\Question;
 
 $factory->define(App\Challenge::class, function (Faker $faker) {
-    $faker->addProvider(new Question($faker));
-
     return [
         'title' => $faker->sentence(2, true),
         'body' => $faker->paragraphs($faker->randomNumber(1), true),
         'body_html' => function(array $challenge) {
             return (new Parsedown())->text($challenge['body']);
         },
-        'questions' => json_encode($faker->questions(3)),
         'skill' => $faker->randomElement(array('Skill 1', 'Skill 2', 'Skill 3', 'Skill 4')),
         'slug' => function(array $challenge) {
             return str_slug($challenge['title']);
