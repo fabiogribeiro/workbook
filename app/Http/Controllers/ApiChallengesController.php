317cc7b03930c047951a6bb37c4197147b15de8d
Fábio Ribeiro <louiskkf@gmail.com>
2019-06-04 04:42:08 +0100

    Remove some code related to challenges api

--- app/Http/Controllers/ApiChallengesController.php
+++ app/Http/Controllers/ApiChallengesController.php
@@ -12,17 +12,4 @@ use App\Jobs\SolveChallenge;
 
 class ApiChallengesController extends Controller
 {
-    /**
-     * Notify that a challenge has been solved.
-     *
-     * @param \Illuminate\Http\Request $request
-     *
-     * @return array
-     */
-    public function solve(Request $request)
-    {
-        SolveChallenge::dispatch($request->challengeId, $request->user());
-
-        return ['result' => true];
-    }
 }
--- routes/api.php
+++ routes/api.php
@@ -16,8 +16,3 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-Route::get('/challenges/{subject}', 'ApiChallengesController@index');
-Route::get('/challenge/{challenge}', 'ApiChallengesController@show');
-
-Route::middleware('auth:api')->put('/challenge/solve', 'ApiChallengesController@solve');
