b40db5be9687b62a801cd0c57f3d4febf41f0548
Fábio Ribeiro <louiskkf@gmail.com>
2019-07-01 16:08:20 +0100

    [Visual] Change default card feedback animation

--- resources/views/partials/subjects.blade.php
+++ resources/views/partials/subjects.blade.php
@@ -8,18 +8,21 @@
       {{ strtoupper($domain) }}
     @endcomponent
 
-   <div class="uk-grid-small uk-child-width-1-3@s uk-child-width-1-4@m uk-text-center uk-text-left@s" uk-grid>
-    @foreach ($subjectList as $subject)
-    <a class="uk-link-reset" href="{{ route('challenges.index', ['domain' => $domain,
-                                                                'subject' => $subject->slug]) }}">
-      <div>
-        <div class="uk-card uk-card-default uk-card-hover uk-card-body">
-          <h3 class="uk-card-title">{{ $subject->title }}</h3>
-          <p>Lorem ipsum</p>
+
+  <div class="uk-margin-small">
+    <div class="uk-grid-small uk-child-width-1-3@s uk-child-width-1-4@m uk-text-center uk-text-left@s" uk-grid>
+      @foreach ($subjectList as $subject)
+      <a class="uk-link-reset uk-button-text" href="{{ route('challenges.index', ['domain' => $domain,
+                                                                  'subject' => $subject->slug]) }}">
+        <div>
+          <div class="uk-card uk-card-default uk-card-hover uk-card-body">
+            <h3 class="uk-card-title uk-text-truncate">{{ $subject->title }}</h3>
+            <p>Lorem ipsum</p>
+          </div>
         </div>
-      </div>
-    </a>
-    @endforeach
-  </div> 
+      </a>
+      @endforeach
+    </div>
+  </div>
   @endforeach
 </div>
