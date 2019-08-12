5eb1490f406ee5a0d3df18da17ef8deef3859ad8
Fábio Ribeiro <louiskkf@gmail.com>
2019-08-12 14:19:48 +0100

    Reindent form pages

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,49 +1,49 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="default-card auth-card col-md-8 col-lg-4">
-            <h2>{{ __('Login') }}</h2>
-
-            <form method="POST" action="{{ route('login') }}">
-                @csrf
-
-                <div class="text-center">
-                    <div>
-                        <input id="email" type="email" class="w-75 default-input{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" placeholder="Email" value="{{ old('email') }}" required>
-
-                        @if ($errors->has('email'))
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $errors->first('email') }}</strong>
-                            </span>
-                        @endif
-                    </div>
-
-                    <div>
-                        <input id="password" type="password" class="w-75 default-input{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" placeholder="Password" required>
-
-                        @if ($errors->has('password'))
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $errors->first('password') }}</strong>
-                            </span>
-                        @endif
-                    </div>
-                </div>
-
-                <div class="text-center">
-                    <button type="submit" class="btn btn-primary">
-                        {{ __('Login') }}
-                    </button>
-
-                    @if (Route::has('password.request'))
-                        <a class="align-bottom btn btn-link" href="{{ route('password.request') }}">
-                            {{ __('Forgot Your Password?') }}
-                        </a>
-                    @endif
-                </div>
-            </form>
+<div >
+  <div class="row justify-content-center">
+    <div class="default-card auth-card col-md-8 col-lg-4">
+      <h2>{{ __('Login') }}</h2>
+      
+      <form method="POST" action="{{ route('login') }}">
+        @csrf
+        
+        <div class="text-center">
+          <div>
+            <input id="email" type="email" class="w-75 default-input{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" placeholder="Email" value="{{ old('email') }}" required>
+            
+            @if ($errors->has('email'))
+            <span class="invalid-feedback" role="alert">
+              <strong>{{ $errors->first('email') }}</strong>
+            </span>
+            @endif
+          </div>
+          
+          <div>
+            <input id="password" type="password" class="w-75 default-input{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" placeholder="Password" required>
+            
+            @if ($errors->has('password'))
+            <span class="invalid-feedback" role="alert">
+              <strong>{{ $errors->first('password') }}</strong>
+            </span>
+            @endif
+          </div>
         </div>
+        
+        <div class="text-center">
+          <button type="submit" class="btn btn-primary">
+            {{ __('Login') }}
+          </button>
+          
+          @if (Route::has('password.request'))
+          <a class="align-bottom btn btn-link" href="{{ route('password.request') }}">
+            {{ __('Forgot Your Password?') }}
+          </a>
+          @endif
+        </div>
+      </form>
     </div>
+  </div>
 </div>
 @endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,46 +2,46 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="default-card auth-card col-md-8 col-lg-4">
-            <h2>{{ __('auth.register') }} </h2>
-
-            <form method="POST" action="{{ route('register') }}">
-                @csrf
-
-                <div class="text-center">
-                    <div>
-                        <input id="email" type="email" class="w-75 default-input{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" placeholder="Email" required>
-
-                        @if ($errors->has('email'))
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $errors->first('email') }}</strong>
-                            </span>
-                        @endif
-                    </div>
-
-                    <div>
-                        <input id="password" type="password" class="w-75 default-input{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" placeholder="Password" required>
-
-                        @if ($errors->has('password'))
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $errors->first('password') }}</strong>
-                            </span>
-                        @endif
-                    </div>
-                </div>
-
-                <div class="text-center">
-                    <button type="submit" class="btn btn-primary">
-                        {{ __('Register') }}
-                    </button>
-
-                    <a class="align-bottom btn btn-link" href="{{ route('login') }}">
-                        {{ __('Have an account? Login') }}
-                    </a>
-                </div>
-            </form>
+  <div class="row justify-content-center">
+    <div class="default-card auth-card col-md-8 col-lg-4">
+      <h2>{{ __('auth.register') }} </h2>
+      
+      <form method="POST" action="{{ route('register') }}">
+        @csrf
+        
+        <div class="text-center">
+          <div>
+            <input id="email" type="email" class="w-75 default-input{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" placeholder="Email" required>
+            
+            @if ($errors->has('email'))
+            <span class="invalid-feedback" role="alert">
+              <strong>{{ $errors->first('email') }}</strong>
+            </span>
+            @endif
+          </div>
+          
+          <div>
+            <input id="password" type="password" class="w-75 default-input{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" placeholder="Password" required>
+            
+            @if ($errors->has('password'))
+            <span class="invalid-feedback" role="alert">
+              <strong>{{ $errors->first('password') }}</strong>
+            </span>
+            @endif
+          </div>
         </div>
+        
+        <div class="text-center">
+          <button type="submit" class="btn btn-primary">
+            {{ __('Register') }}
+          </button>
+          
+          <a class="align-bottom btn btn-link" href="{{ route('login') }}">
+            {{ __('Have an account? Login') }}
+          </a>
+        </div>
+      </form>
     </div>
+  </div>
 </div>
 @endsection
