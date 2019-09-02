74970e9e1077058922206a136ca1296647682779
Fábio Ribeiro <louiskkf@gmail.com>
2019-09-02 17:02:42 +0100

    Delete unnecessary partial

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,7 +3,34 @@
 @section('content')
 
 <div class="uk-padding gray-bg">
-  @include('partials.subjects')
+  <div class="uk-container">
+    <div class="page-description-header">
+      <h3 class="uk-text-bold">Learn, practice or solve challenges</h3>
+    </div>
+
+    @foreach ($subjects as $domain => $subjectList)
+      <h5 class="uk-heading-line">
+        <span>{{ strtoupper($domain) }}</span>
+      </h5>
+
+
+    <div class="uk-margin-small">
+      <div class="uk-grid-small uk-child-width-1-3@s uk-text-center uk-text-left@s" uk-grid>
+        @foreach ($subjectList as $subject)
+        <a class="uk-link-reset uk-button-text" href="{{ route('challenges.index', ['domain' => $domain,
+                                                                    'subject' => $subject->slug]) }}">
+          <div>
+            <div class="uk-card uk-card-default uk-card-hover uk-card-body">
+              <h4 class="uk-text-truncate">{{ $subject->title }}</h4>
+              <p class="uk-text-meta uk-text-truncate">Lorem ipsum</p>
+            </div>
+          </div>
+        </a>
+        @endforeach
+      </div>
+    </div>
+    @endforeach
+  </div>
 </div>
 
 @endsection
