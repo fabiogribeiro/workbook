48a5bd13d63ea60e45f0819411dabb20326141d1
Fábio Ribeiro <louiskkf@gmail.com>
2019-09-06 23:36:01 +0100

    Set challenges as solved when all questions are completed

--- app/Http/Controllers/ApiChallengesController.php
+++ app/Http/Controllers/ApiChallengesController.php
@@ -7,34 +7,46 @@ use App\Question;
 
 class ApiChallengesController extends Controller
 {
-  public function answerQuestion(Request $request)
-  {
-    $question = Question::find($request->id);
-    $answer = $request->answer;
-    $user = $request->user();
+    public function answerQuestion(Request $request)
+    {
+        $question = Question::find($request->id);
+        $challenge = $question->challenge;
+        $answer = $request->answer;
+        $user = $request->user();
 
-    if ($user && $this->isCorrectAnswer($question, $answer)) {
+        if ($user && $this->isCorrectAnswer($question, $answer)) {
 
-      $solvedQuestions = $user->solved_questions ?: [];
-      $solvedQuestions[] = $question->id;
-      $solvedQuestions = array_values(array_unique($solvedQuestions));
+            $solvedQuestions = $user->solved_questions ?: [];
+            $solvedQuestions[] = $question->id;
+            $user->solved_questions = $solvedQuestions;
 
-      $user->solved_questions = $solvedQuestions;
-      $user->save();
+            $unsolvedQuestions = $challenge->questions()
+                                    ->whereNotIn('id', $solvedQuestions)
+                                    ->get();
 
-      return ['result' => true];
+            if ($unsolvedQuestions->isEmpty()) {
+                // No unsolved questions, challenge is solved
+
+                $solvedChallenges = $user->solved_challenges ?: [];
+                $solvedChallenges[] = $challenge->id;
+                $user->solved_challenges = $solvedChallenges;
+            }
+
+            $user->save();
+
+            return ['result' => true];
+        }
+
+        return ['result' => false];
     }
 
-    return ['result' => false];
-  }
-
-  /**
-   * Simple answer checker
-   *
-   * @return bool
-   */
-  private function isCorrectAnswer($question, $answer)
-  {
-    return $question->question_data['answer'] == $answer;
-  }
+    /**
+     * Simple answer checker
+     *
+     * @return bool
+     */
+    private function isCorrectAnswer($question, $answer)
+    {
+        return $question->question_data['answer'] == $answer;
+    }
 }
